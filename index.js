575ed81661d5232e92a70e2661de6b97b1a855c0
EniseK <enisekizilaslan@gmail.com>
2022-12-02 12:41:32 +0100

    added moment timezone function to the cities when clicked

---
+++ index.js
@@ -0,0 +1,28 @@
+function displayTime(event) {
+	if (event.target.value == "Hannover") {
+		alert(
+			moment()
+				.tz("Europe / Berlin")
+				.format("[It is ] dddd, MMMM D , YYYY h:m A [ in Hannover/Germany.]")
+		);
+	}
+
+	if (event.target.value == "Istanbul") {
+		alert(
+			moment()
+				.tz("Europe / Istanbul")
+				.format("[It is ] dddd, MMMM D , YYYY h:m A [ in Istanbul/Turkiye.]")
+		);
+	}
+
+	if (event.target.value == "Michigan") {
+		alert(
+			moment()
+				.tz("US / Michigan")
+				.format("[It is ] dddd, MMMM D , YYYY h:m A [ in Michigan/US.]")
+		);
+	}
+}
+
+let city = document.querySelector("#cityMenu");
+city.addEventListener("click", displayTime);
